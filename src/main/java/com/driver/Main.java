2ae0e93d8835c1a4bed5d1a7feddd4ed6f678739
wambexpeter <wambexpeter@gmail.com>
2023-02-08 14:55:34 +0300

    peter

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+   public static class B extends A{
+        @Override
+                public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+
+    }
+    public static void main(String[] args){
+        A b1 = new B();
+        b1.meth();
+        B b2 = new B();
+        b2.meth();
+    }
   
 }
